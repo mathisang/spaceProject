b1851432121f6f59bfc5e5c4ebf75587f76464f7
TimBlanchard <timothee.blanchard37@gmail.com>
2020-06-11 17:35:31 +0200

    add new json and season timeline

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ import GaugeContext from "./components/Gauge/GaugeContext";
 import Timeline from "./components/Timeline"
 import TimelineContext from "./components/Timeline/TimelineContext";
 import SeasonTimeline from "./components/SeasonTimeline"
+import CardContext from "./components/Card/CardContext";
 
 function App() {
     const [gauge, setGauge] = useState({argent: 50, opinion: 50, recherche: 50});
     const gaugeContextValue = {gauge, setGauge};
     const [timeline, setTimeline] = useState({urss: 5, usa: 0});
     const timelineContextValue = {timeline, setTimeline};
+    const [selectedCardId, setSelectedCardId] = useState(null);
+    const cardContextValue = {selectedCardId, setSelectedCardId};
     return (
+        <CardContext.Provider value={cardContextValue}>
         <GaugeContext.Provider value={gaugeContextValue}>
             <TimelineContext.Provider value={timelineContextValue}>
                 <div className="App">
                     <div className="head-container">
-                    <SeasonTimeline/>
-                    <Gauge/>
+                        <SeasonTimeline/>
+                        <Gauge/>
                     </div>
                     <Card/>
-                    <Timeline />
+                    <Timeline/>
                 </div>
             </TimelineContext.Provider>
         </GaugeContext.Provider>
+        </CardContext.Provider>
     );
 }
 
--- src/components/SeasonTimeline/index.js
+++ src/components/SeasonTimeline/index.js
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import steps from '../../time.json'
+import CardContext from "../Card/CardContext";
 
 
 export default function () {
     const[step, setStep] = useState(0);
+    const {selectedCardId, setSelectedCardId} = useContext(CardContext);
+
+    useEffect(() => {
+        selectedCardId > 0 && setStep(step +1);
+    }, [selectedCardId]);
 
     return (
         <div className="">
