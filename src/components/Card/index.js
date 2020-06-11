40c91a9d8d4e13868674b59a726fa46b1c9d83e4
TimBlanchard <timothee.blanchard37@gmail.com>
2020-06-11 09:26:06 +0200

    modification json paramètre succès / début génération alétoire des cartes (commenté) / début mécanique coséquence

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -7,11 +7,23 @@ import GaugeContext from "../Gauge/GaugeContext";
 
 export default function () {
     const {gauge, setGauge} = useContext(GaugeContext);
-    /*const [cardPercentEffect, setCardPercentEffect] = useState({argent: 0, opinion: 0, recherche: 0});*/
+    const [isSuccess, setSuccess] = useState(null);
+    /*const card = cards.sort(() => Math.random() - Math.random())
+        .find(() => true);*/
 
     useEffect(() => {
         console.log(gauge.argent)
     }, [gauge.argent]);
+
+    function trySuccess() {
+        let r = Math.random();
+        (r < 0.5) ? setSuccess(true) : setSuccess(false);
+    }
+
+    /*useEffect(() => {
+        console.log(card)
+    }, [card]);*/
+
 
     return (
         <div className="">
@@ -20,12 +32,24 @@ export default function () {
                     <div className="card-container" key={index} >
                         {card.text.intitule}
                         <div>
+                            <button onClick={() => {setGauge({argent: gauge.argent + card.value.reponse_un.argent, opinion: gauge.opinion + card.value.reponse_un.opinion, recherche: gauge.recherche + card.value.reponse_un.recherche}); card.consequence.exist && trySuccess();}}>{card.text.reponse_un}</button>
+                            <button onClick={() => setGauge({argent: gauge.argent + card.value.reponse_deux.argent, opinion: gauge.opinion + card.value.reponse_deux.opinion, recherche: gauge.recherche + card.value.reponse_deux.recherche})}>{card.text.reponse_deux}</button>
+                        </div>
+                        {
+                            card.consequence.exist && <div>consequence : {(isSuccess !== null) && ( isSuccess ? (<p>succès</p> /*&& (() => setGauge({argent: gauge.argent + card.consequence.success.argent, opinion: gauge.opinion + card.consequence.success.opinion, recherche: gauge.recherche + card.consequence.success.recherche}))*/ ) : <p>échec</p>)}</div>
+                        }
+                    </div>
+                )
+                }
+                {/*{
+                    <div className="card-container" >
+                        {card.text.intitule}
+                        <div>
                             <button onClick={() => setGauge({argent: gauge.argent + card.value.reponse_un.argent, opinion: gauge.opinion + card.value.reponse_un.opinion, recherche: gauge.recherche + card.value.reponse_un.recherche})}>{card.text.reponse_un}</button>
                             <button onClick={() => setGauge({argent: gauge.argent + card.value.reponse_deux.argent, opinion: gauge.opinion + card.value.reponse_deux.opinion, recherche: gauge.recherche + card.value.reponse_deux.recherche})}>{card.text.reponse_deux}</button>
                         </div>
                     </div>
-                )
-                }
+                }*/}
             </ul>
         </div>
     );
