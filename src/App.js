c36d8ca212a1b05d96db6c32c0e1abaee55a52ba
TimBlanchard <timothee.blanchard37@gmail.com>
2020-06-22 12:18:29 +0200

    add asteroid gltf object and implementation of it in asteroid component on specific route

--- src/App.js
+++ src/App.js
@@ -2,19 +2,33 @@ import React, {useState} from 'react';
 import './App.css';
 import StartCard from "./components/Cards/StartCard";
 import GameOn from "./components/GameOn";
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import Asteroid from '../src/components/Three/Asteroid'
 
 function App() {
+
     const [isGameOn, setGameStatus] = useState(false);
     const handleChange = () => setGameStatus(true);
 
     return (
-        <div className="App">
-                {
-                    isGameOn ? <GameOn/> : <StartCard onChangeStatus={handleChange}/>
-                }
-        </div>
-
+            <Switch>
+                <Route path="/asteroid-test">
+                    <Asteroid/>
+                </Route>
+                <Route path="/">
+                    <div className="App">
+                        {
+                            isGameOn ? <GameOn/> : <StartCard onChangeStatus={handleChange}/>
+                        }
+                    </div>
+                </Route>
+            </Switch>
     );
 }
 
-export default App;
+export default () => (
+    <Router>
+        <App />
+    </Router>
+)
+
