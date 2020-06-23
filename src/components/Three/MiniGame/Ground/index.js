a236a4a010bec5556fa7f8e2342289c5c9546831
TimBlanchard <timothee.blanchard37@gmail.com>
2020-06-23 19:27:08 +0200

    [package] add react-use-gesture

--- src/components/Three/MiniGame/Ground/index.js
+++ src/components/Three/MiniGame/Ground/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { usePlane } from 'use-cannon'
 
 const Ground = (props) => {
-    const [ref] = usePlane(() => ({ rotation: [-Math.PI/ 2, 0, 0], position: [0, -3, 0], ...props }))
+    const [ref] = usePlane(() => ({ rotation: [-Math.PI/ 2, 0, 0], position: [0, -5, 0], ...props }))
     return (
         <mesh ref={ref} receiveShadow>
             <planeBufferGeometry attach="geometry" args={[10, 10]} />
--- src/components/Three/MiniGame/Rocket/index.js
+++ src/components/Three/MiniGame/Rocket/index.js
@@ -6,7 +6,7 @@ import { useFrame } from "react-three-fiber"
 export default function Rocket({args = [1, 2, 0]}) {
     const [ref, api] = useBox(() => ({ args }))
     useFrame(state => {
-        api.position.set((state.mouse.x * state.viewport.width) / 2, (state.mouse.y * state.viewport.height) / 2, 0)
+        api.position.set((state.mouse.x * state.viewport.width) / 2, -4, 0)
     })
     return (
         <mesh>
