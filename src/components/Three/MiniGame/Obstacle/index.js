91ca070f63b985c7346d365798e785a417626253
TimBlanchard <timothee.blanchard37@gmail.com>
2020-06-24 17:28:13 +0200

    [three] add lifePoints and reduce when obstacle hit

--- src/components/Three/MiniGame/Obstacle/index.js
+++ src/components/Three/MiniGame/Obstacle/index.js
@@ -3,7 +3,11 @@ import { useSphere } from "use-cannon";
 import { Sphere } from "drei";
 
 export default function Obstacle(props) {
-  const [ref] = useSphere(() => ({ mass: 1, position: [0, 5, 0], ...props }));
+  const [ref] = useSphere(() => ({
+    mass: 1,
+    position: [0, 5, 0],
+    ...props,
+  }));
   return (
     <mesh>
       <Sphere receiveShadow castShadow ref={ref} args={[0.5]}>
--- src/components/Three/MiniGame/index.js
+++ src/components/Three/MiniGame/index.js
@@ -4,34 +4,45 @@ import { Physics } from "use-cannon";
 import Rocket from "./Rocket";
 import Obstacle from "./Obstacle";
 import Ground from "./Ground";
+import "./miniGame.scss";
 
 export default () => {
   const [isTouched, setTouched] = useState(false);
+  const [lifePoints, setLifePoints] = useState(4);
 
   useEffect(() => {
+    if (isTouched) {
+      setTimeout(() => {
+        setLifePoints(lifePoints - 1);
+        setTouched(false);
+      }, 1000);
+    }
     console.log(isTouched);
   }, [isTouched]);
 
   return (
-    <Canvas
-      shadowMap
-      sRGB
-      gl={{ alpha: false }}
-      camera={{ position: [0, 1, 7] }}
-    >
-      <ambientLight intensity={0.2} />
-      <spotLight
-        position={[30, 10, 10]}
-        penumbra={1}
-        intensity={0.5}
-        castShadow
-      />
-      <Physics>
-        <Rocket isTouched={isTouched} setTouched={setTouched} />
-        <Obstacle position={[3, 10, 0]} />
-        <Obstacle position={[0, 10, 0]} />
-        {/*<Ground />*/}
-      </Physics>
-    </Canvas>
+    <div className="minigame-container">
+      <h1>Life : {lifePoints}</h1>
+      <Canvas
+        shadowMap
+        sRGB
+        gl={{ alpha: false }}
+        camera={{ position: [0, 1, 7] }}
+      >
+        <ambientLight intensity={0.2} />
+        <spotLight
+          position={[30, 10, 10]}
+          penumbra={1}
+          intensity={0.5}
+          castShadow
+        />
+        <Physics>
+          <Rocket isTouched={isTouched} setTouched={setTouched} />
+          <Obstacle position={[3, 10, 0]} />
+          <Obstacle position={[0, 10, 0]} />
+          {/*<Ground />*/}
+        </Physics>
+      </Canvas>
+    </div>
   );
 };
