4da859893a4115eee0bb02abd770665424bc525c
Mathis Angibaud <m.angibaudmathis@gmail.com>
2020-06-24 16:36:29 +0200

    [json] New structure & new cards

--- src/components/Three/MiniGame/Ground/index.js
+++ src/components/Three/MiniGame/Ground/index.js
@@ -1,15 +1,14 @@
-import React from "react"
-import { useSphere } from 'use-cannon'
-import { Sphere} from "drei"
+import React from "react";
+import { useSphere } from "use-cannon";
+import { Sphere } from "drei";
 
 export default function Obstacle(props) {
-    const [ref] = useSphere(() => ({ mass: 1, position: [0, 5, 0], ...props }))
-    return (
-        <mesh>
-        <Sphere receiveShadow castShadow ref={ref} args={[0.5]}>
+  const [ref] = useSphere(() => ({ mass: 1, position: [0, 5, 0], ...props }));
+  return (
+    <mesh>
+      <Sphere receiveShadow castShadow ref={ref} args={[0.5]}>
         <meshLambertMaterial attach="material" color="grey" />
-        </Sphere>
-        </mesh>
-)
+      </Sphere>
+    </mesh>
+  );
 }
-
--- src/components/Three/MiniGame/Rocket/index.js
+++ src/components/Three/MiniGame/Rocket/index.js
@@ -1,22 +1,20 @@
-import React from "react"
-import {useBox} from 'use-cannon'
-import {Box} from "drei"
-import {useFrame} from "react-three-fiber"
-import {useDrag} from "react-use-gesture";
+import React from "react";
+import { useBox } from "use-cannon";
+import { Box } from "drei";
+import { useFrame } from "react-three-fiber";
+import { useDrag } from "react-use-gesture";
 
+export default function Rocket({ args = [1, 2, 1] }) {
+  const [ref, api] = useBox(() => ({ args }));
 
-
-export default function Rocket({args = [1, 2, 1]}) {
-    const [ref, api] = useBox(() => ({args}))
-
-    useFrame(state => {
-        api.position.set((state.mouse.x * state.viewport.width) / 2, -4, 0)
-    })
-    return (
-        <mesh>
-            <Box ref={ref} args={args}>
-                <meshStandardMaterial attach="material" color="grey"/>
-            </Box>
-        </mesh>
-    )
+  useFrame((state) => {
+    api.position.set((state.mouse.x * state.viewport.width) / 2, -4, 0);
+  });
+  return (
+    <mesh>
+      <Box ref={ref} args={args}>
+        <meshStandardMaterial attach="material" color="grey" />
+      </Box>
+    </mesh>
+  );
 }
