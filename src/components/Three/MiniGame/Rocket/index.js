13413421998171c2a876b3eb95d50561d39fec24
TimBlanchard <timothee.blanchard37@gmail.com>
2020-06-26 13:17:43 +0200

    [three] rescaling rocket

--- src/components/Three/MiniGame/Rocket/index.js
+++ src/components/Three/MiniGame/Rocket/index.js
@@ -6,6 +6,7 @@ import { useSpring, a } from "react-spring/three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export default function Rocket({ propsCannon, setTouched, isTouched }) {
+  // use cannon box declaration
   const [ref, api] = useBox(() => ({
     args: [1, 3, 1],
     onCollide: () => setTouched(true),
@@ -14,6 +15,7 @@ export default function Rocket({ propsCannon, setTouched, isTouched }) {
   const propsSpring = useSpring({
     color: isTouched ? "red" : "grey",
   });
+  // gltf model load
   const { nodes } = useLoader(
     GLTFLoader,
     "three/miniGameSpaceship/spaceShip.glb"
@@ -25,13 +27,18 @@ export default function Rocket({ propsCannon, setTouched, isTouched }) {
   }, []);
 
   useFrame((state) => {
-    api.position.set((state.mouse.x * state.viewport.width) / 2, -4, 0);
+    api.position.set((state.mouse.x * state.viewport.width) / 2, -2, 0);
   });
   return (
-    <a.mesh ref={ref}>
-      <group ref={groupModel}>
+    <a.mesh ref={ref} color={propsSpring.color}>
+      <group ref={groupModel} scale={[0.2, 0.2, 0.2]}>
         <mesh visible geometry={nodes.Cylinder001_0.geometry}>
-          <a.meshPhysicalMaterial attach="material" color={propsSpring.color} />
+          <meshPhysicalMaterial
+            attach="material"
+            color="white"
+            roughness={0.3}
+            metalness={0.3}
+          />
         </mesh>
         <mesh visible geometry={nodes.Cylinder001_1.geometry}>
           <meshStandardMaterial
