93a2e4286977a264697672fa3384efed84b1a563
TimBlanchard <timothee.blanchard37@gmail.com>
2020-06-26 14:17:12 +0200

    [three] add colors on rocket

--- src/components/Three/MiniGame/Rocket/index.js
+++ src/components/Three/MiniGame/Rocket/index.js
@@ -8,13 +8,10 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 export default function Rocket({ propsCannon, setTouched, isTouched }) {
   // use cannon box declaration
   const [ref, api] = useBox(() => ({
-    args: [1, 3, 1],
+    args: [1, 0, 1],
     onCollide: () => setTouched(true),
     ...propsCannon,
   }));
-  const propsSpring = useSpring({
-    color: isTouched ? "red" : "grey",
-  });
   // gltf model load
   const { nodes } = useLoader(
     GLTFLoader,
@@ -29,29 +26,37 @@ export default function Rocket({ propsCannon, setTouched, isTouched }) {
   useFrame((state) => {
     api.position.set((state.mouse.x * state.viewport.width) / 2, -2, 0);
   });
+  const propsSpring = useSpring({
+    colorHit: "red",
+  });
+
   return (
-    <a.mesh ref={ref} color={propsSpring.color}>
+    <mesh ref={ref}>
       <group ref={groupModel} scale={[0.2, 0.2, 0.2]}>
         <mesh visible geometry={nodes.Cylinder001_0.geometry}>
           <meshPhysicalMaterial
             attach="material"
-            color="white"
+            color={nodes.Cylinder001_0.material.color}
             roughness={0.3}
             metalness={0.3}
           />
         </mesh>
         <mesh visible geometry={nodes.Cylinder001_1.geometry}>
-          <meshStandardMaterial
+          <meshPhysicalMaterial
             attach="material"
-            color="white"
+            color={nodes.Cylinder001_1.material.color}
             roughness={0.3}
             metalness={0.3}
           />
         </mesh>
         <mesh visible geometry={nodes.Cylinder001_2.geometry}>
-          <meshStandardMaterial
+          <a.meshStandardMaterial
             attach="material"
-            color="white"
+            color={
+              isTouched
+                ? propsSpring.colorHit
+                : nodes.Cylinder001_2.material.color
+            }
             roughness={0.3}
             metalness={0.3}
           />
@@ -59,15 +64,19 @@ export default function Rocket({ propsCannon, setTouched, isTouched }) {
         <mesh visible geometry={nodes.Cylinder001_3.geometry}>
           <meshStandardMaterial
             attach="material"
-            color="white"
+            color={nodes.Cylinder001_3.material.color}
             roughness={0.3}
             metalness={0.3}
           />
         </mesh>
         <mesh visible geometry={nodes.Cylinder001_4.geometry}>
-          <meshStandardMaterial
+          <a.meshStandardMaterial
             attach="material"
-            color="white"
+            color={
+              isTouched
+                ? propsSpring.colorHit
+                : nodes.Cylinder001_4.material.color
+            }
             roughness={0.3}
             metalness={0.3}
           />
@@ -76,6 +85,6 @@ export default function Rocket({ propsCannon, setTouched, isTouched }) {
       {/*<Box ref={ref} args={[1, 3, 1]}>
         <a.meshPhysicalMaterial attach="material" color={propsSpring.color} />
       </Box>*/}
-    </a.mesh>
+    </mesh>
   );
 }
