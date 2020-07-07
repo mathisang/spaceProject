a7769a899a43c5e57871bfa031c9c994e671e3bb
TimBlanchard <timothee.blanchard37@gmail.com>
2020-07-07 15:20:29 +0200

    [three] adapt value

--- src/components/Three/MoonGame/Scene/Moon/index.js
+++ src/components/Three/MoonGame/Scene/Moon/index.js
@@ -6,7 +6,7 @@ export default function Moon() {
   const map = useLoader(THREE.TextureLoader, "three/moongame/moon/moon.jpg");
 
   return (
-    <mesh position={[0, -16, 0]} scale={[30, 30, 1]} receiveShadow castShadow>
+    <mesh position={[0, -16, 0]} scale={[25, 25, 1]} receiveShadow castShadow>
       <sphereBufferGeometry attach="geometry" args={[1, 64, 64]} />
       <meshStandardMaterial attach="material" map={map} />
     </mesh>
